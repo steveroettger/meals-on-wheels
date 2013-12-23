598f4d5f9415d499a8ce31c224a8c0d55304f684
steveroettger <steve@steveroettger.com>
2013-12-23 10:37:36 -0500

    upload basic site files

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<title>Meals On Wheels</title>
+	<!--[if lt IE 10]>
+	      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+	 <![endif]-->
+	
+	<link rel="icon" type="image/png" href="/images/favicon.png">
+
+	<!-- FONTS -->	
+
+	<!-- STYLESHEETS -->
+	<link rel="stylesheet" type="text/css" href="/css/custom.css">
+	
+	<!-- JAVASCRIPTS -->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/site.js"></script>
+
+</head>
+<body>
+
+<div id="header">
+
+</div><!-- END #header -->
+	
